283daf10552d3e67fe2bfc26bd0281c2480e71b3
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-04-03 00:30:13 +0530

    refactor TravelBookingTest

--- src/test/kotlin/com/taxifare/TravelBookingTest.kt
+++ src/test/kotlin/com/taxifare/TravelBookingTest.kt
@@ -2,18 +2,18 @@ package com.taxifare
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class TravelBookingTest {
 
     @Test
-    @Disabled("Refactoring Distance calculator")
     fun `should generate fare from booking`() {
         val expected = "Your fare is 20"
-        val coronaPatients = Booking("Earth", "Heaven", 2)
+        val source = "Earth"
+        val destination = "Heaven"
+        val coronaPatients = Booking(source, destination, 2)
         val calFare: FareGenerator = { distance, headCount -> "Your fare is ${distance.distance * headCount}" }
-        val calDistance: DistanceCalculator = { _ -> TODO("not implemented") }
+        val calDistance: DistanceCalculator = { _ -> Distance(source, destination, 10) }
 
         val taxi = TravelBooking(calFare, calDistance)
 
