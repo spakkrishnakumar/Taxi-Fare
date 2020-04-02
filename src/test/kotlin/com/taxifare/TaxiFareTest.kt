fc0f5672ceaabe15e44fd48737a86b422850b21c
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-04-03 00:34:24 +0530

    implement TaxiFareTest (CRT) to pass

--- src/test/kotlin/com/taxifare/TaxiFareTest.kt
+++ src/test/kotlin/com/taxifare/TaxiFareTest.kt
@@ -2,15 +2,13 @@ package com.taxifare
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import sampleInput_1
 
 class TaxiFareTest {
 
     @Test
-    @Disabled("Customer Requirement Failing Test")
-    internal fun `generate taxi fare for a booking`() {
+    fun `generate taxi fare for a booking`() {
         val input = sampleInput_1
         val expected = sampleOutput_1
         val taxiFareCal: FareGenerator = TaxiFareGenerator()
