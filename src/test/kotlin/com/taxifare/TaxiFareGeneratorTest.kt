03657dc7c7a4bec8077a1717ef89f0012f387c28
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-04-02 23:56:05 +0530

    change expected data for "should calculate taxi fare using distance and headcount"

--- src/test/kotlin/com/taxifare/TaxiFareGeneratorTest.kt
+++ src/test/kotlin/com/taxifare/TaxiFareGeneratorTest.kt
@@ -2,13 +2,15 @@ package com.taxifare
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class TaxiFareGeneratorTest {
 
     @Test
+    @Disabled("Formatting the needs to be implemented")
     fun `should calculate taxi fare using distance and headcount`() {
-        val expected = "2500"
+        val expected = sampleOutput_1
         val distance = 200
         val headCount = 2
         val fareGenerator = TaxiFareGenerator()
