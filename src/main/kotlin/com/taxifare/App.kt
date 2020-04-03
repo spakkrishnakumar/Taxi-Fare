37850237f077372d702333fb6ee77bb31a7d6d95
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-04-03 08:53:26 +0530

    create runnable Console Application

--- src/main/kotlin/com/taxifare/App.kt
+++ src/main/kotlin/com/taxifare/App.kt
@@ -2,6 +2,38 @@ package com.taxifare
 
 fun main() {
 
+    val taxiFareCal: FareGenerator = TaxiFareGenerator(DefaultFareFormatter())
+    val groundDistanceRepo: DistanceRepository = FileDistanceRepository("sampleDB.txt")
+    val distanceCal: DistanceCalculator = GroundDistanceCalculator(groundDistanceRepo)
+    val taxiBooking = TravelBooking(taxiFareCal, distanceCal)
+
+    val app = ConsoleApp(taxiBooking)
+
+    app()
+}
+
+class ConsoleApp(
+    private val bookingService: Bookable
+) : Application() {
+
+    override fun book(): Booking {
+        println("Enter Booking Info: ")
+        print("Enter From: ")
+        val from = readLine().orEmpty()
+        print("Enter To: ")
+        val to = readLine().orEmpty()
+        print("Enter No of travellers: ")
+        val headCount = readLine()?.toInt() ?: 0
+        return Booking(from, to, headCount)
+    }
+
+    override fun generate(booking: Booking): String = bookingService(booking).also(::printFare)
+
+    private fun printFare(fare: String) {
+        println("\nYour Fare is: ")
+        println(fare)
+    }
+
 }
 
 
