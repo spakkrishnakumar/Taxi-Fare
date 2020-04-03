cf870004fabaac9468a2ca16fe57903853e105f5
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-04-03 10:42:35 +0530

    refactor TaxiFareTest - convert to parameterized test

--- src/test/kotlin/com/taxifare/crt/TaxiFareTest.kt
+++ src/test/kotlin/com/taxifare/crt/TaxiFareTest.kt
@@ -4,16 +4,18 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.taxifare.*
 import com.taxifare.testdata.sampleOutput_1
+import com.taxifare.testdata.sampleOutput_2
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import sampleInput_1
+import sampleInput_2
 
 class TaxiFareTest {
 
     @ParameterizedTest
-    @ValueSource(strings = [sampleInput_1])
-    fun `generate taxi fare for a booking`(input: String) {
-        val expected = sampleOutput_1
+    @MethodSource("sampleInputAndOutput")
+    fun `generate taxi fare for a booking`(input: String, output: String) {
 
         val taxiFareCal: FareGenerator = TaxiFareGenerator(DefaultFareFormatter())
         val groundDistanceRepo: DistanceRepository = FileDistanceRepository("sampleDB.txt")
@@ -23,7 +25,7 @@ class TaxiFareTest {
 
         val actual = ula(booking(input))
 
-        assertThat(actual, equalTo(expected))
+        assertThat(actual, equalTo(output))
     }
 
     private fun booking(input: String): Booking = input
@@ -34,4 +36,13 @@ class TaxiFareTest {
         .toBooking()
 
     private fun List<String>.toBooking(): Booking = Booking(get(0).trim(), get(1).trim(), get(2).trim().toInt())
+
+    companion object {
+        @JvmStatic
+        fun sampleInputAndOutput() = listOf(
+            Arguments.of(sampleInput_1, sampleOutput_1),
+            Arguments.of(sampleInput_2, sampleOutput_2)
+        )
+    }
+
 }
--- src/test/kotlin/com/taxifare/testdata/sampleOutput.kt
+++ src/test/kotlin/com/taxifare/testdata/sampleOutput.kt
@@ -8,3 +8,12 @@ Destination: NASIK
 Kms: 200
 No. of travellers = 2
 Total = 2500 INR""".trimMargin()
+
+val sampleOutput_2 = """
+Taxi Ticket
+-----------
+Source: NASIK
+Destination: PUNE
+Kms: 200
+No. of travellers = 2
+Total = 2500 INR""".trimMargin()
